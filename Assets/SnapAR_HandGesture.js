50f68c6df446eda92273b1c5d1fe1c53a0b46866
Hai Le <lehoanghai03101997@gmail.com>
2026-03-15 19:41:08 -0400

    Fixed Minor code issues

--- Assets/SnapAR_HandGesture.js
+++ Assets/SnapAR_HandGesture.js
@@ -24,7 +24,6 @@ var hideEvents = {};
 // SHOW VISUAL TEMPORARILY
 // ----------------------------
 function showPopupTemporarily(obj, seconds) {
-
     if (!obj) {
         return;
     }
@@ -48,17 +47,20 @@ function showPopupTemporarily(obj, seconds) {
 
 
 // ----------------------------
-// SEND TO RECIPE MANAGER (NO NEED SINCE THIS IS BASED ON Hai's SCRIPT)
+// SEND TO RECIPE MANAGER
+// Keep this for later if needed
 // ----------------------------
-/* function notifyRecipeManager(gestureName){
-
+function notifyRecipeManager(gestureName) {
     if (!script.recipeManager) {
-        print("RecipeManager missing");
+        return;
+    }
+
+    if (!script.recipeManager.onGestureRecognized) {
         return;
     }
 
     script.recipeManager.onGestureRecognized(gestureName);
-} */
+}
 
 
 // ----------------------------
@@ -66,134 +68,121 @@ function showPopupTemporarily(obj, seconds) {
 // ----------------------------
 if (!script.tracker) {
     print("Tracker not assigned");
+} else {
+
+    // ----------------------------
+    // OPEN HAND
+    // ----------------------------
+    script.tracker.registerDescriptorStart("open", function () {
+        print("Open hand detected");
+
+        if (script.soundOpen) {
+            script.soundOpen.play(1);
+        }
+
+        if (script.visualOpen) {
+            showPopupTemporarily(script.visualOpen, 3);
+        }
+
+        // notifyRecipeManager("open");
+    });
+
+
+    // ----------------------------
+    // CLOSE FIST
+    // ----------------------------
+    script.tracker.registerDescriptorStart("close", function () {
+        print("Close hand detected");
+
+        if (script.soundClose) {
+            script.soundClose.play(1);
+        }
+
+        if (script.visualClose) {
+            showPopupTemporarily(script.visualClose, 3);
+        }
+
+        // notifyRecipeManager("close");
+    });
+
+
+    // ----------------------------
+    // VICTORY SIGN
+    // ----------------------------
+    script.tracker.registerDescriptorStart("victory", function () {
+        print("Victory detected");
+
+        if (script.soundVictory) {
+            script.soundVictory.play(1);
+        }
+
+        if (script.visualVictory) {
+            showPopupTemporarily(script.visualVictory, 3);
+        }
+
+        // notifyRecipeManager("victory");
+    });
+
+
+    // ----------------------------
+    // INDEX FINGER
+    // ----------------------------
+    script.tracker.registerDescriptorStart("index_finger", function () {
+        print("Index finger detected");
+
+        if (script.soundIndex) {
+            script.soundIndex.play(1);
+        }
+
+        if (script.visualIndex) {
+            showPopupTemporarily(script.visualIndex, 3);
+        }
+
+        // notifyRecipeManager("index_finger");
+    });
+
+
+    // ----------------------------
+    // HORNS
+    // ----------------------------
+    script.tracker.registerDescriptorStart("horns", function () {
+        print("Horns gesture detected");
+
+        if (script.soundHorns) {
+            script.soundHorns.play(1);
+        }
+
+        if (script.visualHorns) {
+            showPopupTemporarily(script.visualHorns, 3);
+        }
+
+        // notifyRecipeManager("horns");
+    });
+
+
+    // ----------------------------
+    // THUMB UP
+    // ----------------------------
+    script.tracker.registerDescriptorStart("thumb", function () {
+        print("Thumb gesture detected");
+
+        if (script.soundThumb) {
+            script.soundThumb.play(1);
+        }
+
+        if (script.visualThumb) {
+            showPopupTemporarily(script.visualThumb, 3);
+        }
+
+        // notifyRecipeManager("thumb");
+    });
 }
-
-
-// ----------------------------
-// OPEN HAND
-// ----------------------------
-script.tracker.registerDescriptorStart("open", function(){
-
-    print("Open hand detected");
-
-    if (script.soundOpen) {
-        script.soundOpen.play(1);
-    }
-
-    if (script.visualOpen) {
-        showPopupTemporarily(script.visualOpen, 3);
-    }
-
-    //notifyRecipeManager("open");
-
-});
-
-
-// ----------------------------
-// CLOSE FIST
-// ----------------------------
-script.tracker.registerDescriptorStart("close", function(){
-
-    print("Close hand detected");
-
-    if (script.soundClose) {
-        script.soundClose.play(1);
-    }
-
-    if (script.visualClose) {
-        showPopupTemporarily(script.visualClose, 3);
-    }
-
-    //notifyRecipeManager("close");
-
-});
-
-
-// ----------------------------
-// VICTORY SIGN
-// ----------------------------
-script.tracker.registerDescriptorStart("victory", function(){
-
-    print("Victory detected");
-
-    if (script.soundVictory) {
-        script.soundVictory.play(1);
-    }
-
-    if (script.visualVictory) {
-        showPopupTemporarily(script.visualVictory, 3);
-    }
-
-    //notifyRecipeManager("victory");
-
-});
-
-
-// ----------------------------
-// INDEX FINGER
-// ----------------------------
-script.tracker.registerDescriptorStart("index_finger", function(){
-
-    print("Index finger detected");
-
-    if (script.soundIndex) {
-        script.soundIndex.play(1);
-    }
-
-    if (script.visualIndex) {
-        showPopupTemporarily(script.visualIndex, 3);
-    }
-
-    //notifyRecipeManager("index_finger");
-
-});
-
-
-// ----------------------------
-// HORNS
-// ----------------------------
-script.tracker.registerDescriptorStart("horns", function(){
-
-    print("Horns gesture detected");
-
-    if (script.soundHorns) {
-        script.soundHorns.play(1);
-    }
-
-    if (script.visualHorns) {
-        showPopupTemporarily(script.visualHorns, 3);
-    }
-
-    //notifyRecipeManager("horns");
-
-});
-
-
-// ----------------------------
-// THUMB UP
-// ----------------------------
-script.tracker.registerDescriptorStart("thumb", function(){
-
-    print("Thumb gesture detected");
-
-    if (script.soundThumb) {
-        script.soundThumb.play(1);
-    }
-
-    if (script.visualThumb) {
-        showPopupTemporarily(script.visualThumb, 3);
-    }
-
-    //notifyRecipeManager("thumb");
-
-});
 
 
 // ----------------------------
 // OPTIONAL DESKTOP TESTING
 // ----------------------------
-script.createEvent("TapEvent").bind(function(){
-
+script.createEvent("TapEvent").bind(function () {
     print("Tap test -> open");
 
     if (script.soundOpen) {
@@ -204,6 +193,5 @@ script.createEvent("TapEvent").bind(function(){
         showPopupTemporarily(script.visualOpen, 1);
     }
 
-    notifyRecipeManager("open");
-
+    // notifyRecipeManager("open");
 });
