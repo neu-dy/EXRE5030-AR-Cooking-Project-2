465a27469ac4ca787f215c737fd8ec44ce26fa48
Hai Le <lehoanghai03101997@gmail.com>
2026-03-15 19:33:55 -0400

    Gesture Tracker with HandGesture Script
    Added Object Tracker into the Camera and attached the HandGesture Script to Gesture Tracker object.

---
+++ Assets/SnapAR_HandGesture.js
@@ -0,0 +1,209 @@
+//@input Component.ObjectTracking tracker
+//@input Component.ScriptComponent recipeManager
+
+//@input Component.AudioComponent soundOpen
+//@input Component.AudioComponent soundClose
+//@input Component.AudioComponent soundVictory
+//@input Component.AudioComponent soundIndex
+//@input Component.AudioComponent soundHorns
+//@input Component.AudioComponent soundThumb
+
+//@input SceneObject visualOpen
+//@input SceneObject visualClose
+//@input SceneObject visualVictory
+//@input SceneObject visualIndex
+//@input SceneObject visualHorns
+//@input SceneObject visualThumb
+
+print("Phone HandGesture loaded");
+
+var hideEvents = {};
+
+
+// ----------------------------
+// SHOW VISUAL TEMPORARILY
+// ----------------------------
+function showPopupTemporarily(obj, seconds) {
+
+    if (!obj) {
+        return;
+    }
+
+    obj.enabled = true;
+
+    if (hideEvents[obj.name]) {
+        hideEvents[obj.name].enabled = false;
+    }
+
+    var event = script.createEvent("DelayedCallbackEvent");
+
+    event.bind(function () {
+        obj.enabled = false;
+    });
+
+    event.reset(seconds);
+
+    hideEvents[obj.name] = event;
+}
+
+
+// ----------------------------
+// SEND TO RECIPE MANAGER (NO NEED SINCE THIS IS BASED ON Hai's SCRIPT)
+// ----------------------------
+/* function notifyRecipeManager(gestureName){
+
+    if (!script.recipeManager) {
+        print("RecipeManager missing");
+        return;
+    }
+
+    script.recipeManager.onGestureRecognized(gestureName);
+} */
+
+
+// ----------------------------
+// TRACKER SAFETY
+// ----------------------------
+if (!script.tracker) {
+    print("Tracker not assigned");
+}
+
+
+// ----------------------------
+// OPEN HAND
+// ----------------------------
+script.tracker.registerDescriptorStart("open", function(){
+
+    print("Open hand detected");
+
+    if (script.soundOpen) {
+        script.soundOpen.play(1);
+    }
+
+    if (script.visualOpen) {
+        showPopupTemporarily(script.visualOpen, 3);
+    }
+
+    //notifyRecipeManager("open");
+
+});
+
+
+// ----------------------------
+// CLOSE FIST
+// ----------------------------
+script.tracker.registerDescriptorStart("close", function(){
+
+    print("Close hand detected");
+
+    if (script.soundClose) {
+        script.soundClose.play(1);
+    }
+
+    if (script.visualClose) {
+        showPopupTemporarily(script.visualClose, 3);
+    }
+
+    //notifyRecipeManager("close");
+
+});
+
+
+// ----------------------------
+// VICTORY SIGN
+// ----------------------------
+script.tracker.registerDescriptorStart("victory", function(){
+
+    print("Victory detected");
+
+    if (script.soundVictory) {
+        script.soundVictory.play(1);
+    }
+
+    if (script.visualVictory) {
+        showPopupTemporarily(script.visualVictory, 3);
+    }
+
+    //notifyRecipeManager("victory");
+
+});
+
+
+// ----------------------------
+// INDEX FINGER
+// ----------------------------
+script.tracker.registerDescriptorStart("index_finger", function(){
+
+    print("Index finger detected");
+
+    if (script.soundIndex) {
+        script.soundIndex.play(1);
+    }
+
+    if (script.visualIndex) {
+        showPopupTemporarily(script.visualIndex, 3);
+    }
+
+    //notifyRecipeManager("index_finger");
+
+});
+
+
+// ----------------------------
+// HORNS
+// ----------------------------
+script.tracker.registerDescriptorStart("horns", function(){
+
+    print("Horns gesture detected");
+
+    if (script.soundHorns) {
+        script.soundHorns.play(1);
+    }
+
+    if (script.visualHorns) {
+        showPopupTemporarily(script.visualHorns, 3);
+    }
+
+    //notifyRecipeManager("horns");
+
+});
+
+
+// ----------------------------
+// THUMB UP
+// ----------------------------
+script.tracker.registerDescriptorStart("thumb", function(){
+
+    print("Thumb gesture detected");
+
+    if (script.soundThumb) {
+        script.soundThumb.play(1);
+    }
+
+    if (script.visualThumb) {
+        showPopupTemporarily(script.visualThumb, 3);
+    }
+
+    //notifyRecipeManager("thumb");
+
+});
+
+
+// ----------------------------
+// OPTIONAL DESKTOP TESTING
+// ----------------------------
+script.createEvent("TapEvent").bind(function(){
+
+    print("Tap test -> open");
+
+    if (script.soundOpen) {
+        script.soundOpen.play(1);
+    }
+
+    if (script.visualOpen) {
+        showPopupTemporarily(script.visualOpen, 1);
+    }
+
+    notifyRecipeManager("open");
+
+});
